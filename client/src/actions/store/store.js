e050d1ef019a3595977d4fc1af1c29c9f222cf32
SanjayMalviya20 <malviyasanjay8875438@gmail.com.com>
2024-10-29 17:03:16 +0530

    new changes

--- client/src/actions/store/store.js
+++ client/src/actions/store/store.js
@@ -29,10 +29,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../action";
 import postSlice  from "../postaction";
+import { combineReducers } from "@reduxjs/toolkit";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+  
+  const rootReducer = combineReducers({
+   user:userSlice,
+   posts:postSlice,
+  })
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-    reducer:{
-        user:userSlice,
-        posts:postSlice,
-    }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   
 })
+
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './actions/store/store.js'
-
+import { PersistGate } from 'redux-persist/lib/integration/react.js'
+import persistStore from 'redux-persist/es/persistStore'
+let persistor = persistStore(store)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-        <App />
-    </Provider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor} >
+      <App />
+    </PersistGate>
+  </Provider>,
   </StrictMode>,
 )
